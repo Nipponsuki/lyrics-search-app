d5be7aab899d69b222dd610ec4ca9664fff01fd9
Alex <alex.alexeev23061991@gmail.com>
2019-01-30 19:24:17 +0300

    Updates

---
+++ precache-manifest.dc733e3c3f6f1672c83407ae69539125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d7599b898596d2624684f466480a1e6",
+    "url": "/Nipponsuki/lyrics-search-app/static/media/spinner.2d7599b8.gif"
+  },
+  {
+    "revision": "dd7364ab39053fcdc015",
+    "url": "/Nipponsuki/lyrics-search-app/static/js/runtime~main.dd7364ab.js"
+  },
+  {
+    "revision": "2a4b3204e42f8475535b",
+    "url": "/Nipponsuki/lyrics-search-app/static/js/main.2a4b3204.chunk.js"
+  },
+  {
+    "revision": "7d656c559f3b8bfa3616",
+    "url": "/Nipponsuki/lyrics-search-app/static/js/1.7d656c55.chunk.js"
+  },
+  {
+    "revision": "2a4b3204e42f8475535b",
+    "url": "/Nipponsuki/lyrics-search-app/static/css/main.58dd30b2.chunk.css"
+  },
+  {
+    "revision": "8ca64d48b3024473c82958da1b3a981e",
+    "url": "/Nipponsuki/lyrics-search-app/index.html"
+  }
+];
